a90bcf3411d3561d08ed69da75a7fe18ec2fa5cc
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-04-12 12:29:37 -0500

    notes done, libray loaded

---
+++ js/results.js
@@ -0,0 +1,39 @@
+/*
+TO DO: get info from local storage and prase (JSON.Prase......)
+TO DO: fetch from api based off of saved results we just prased 
+
+go in make sure you are passing the correct parameters to the api using spilt and repalcing (for hints look at the mini prject )
+
+
+TO DO: dynamtically display each pet with stying and information, 
+    things to display (if time display img, if not dont worry about it we can give them a link t othe img instead of displaying each img and saving it and all that)
+    we do want what age the cat will be, we want the name, display breed, display color, display discription, display address and conteact info, (inside of a contact object we could display)  display cat disability
+
+    capture abd put into an different object called
+    let favResults = {
+      name: text
+      breed: text
+      color: text
+      img: url, text 
+      discr: text
+      contact = {
+        address: text
+        phoneNum: int
+        email: text
+      }
+      disabilty: boolean
+
+    }
+
+    TO DO: dynamtically display this object, 
+    TO DO: style this guy with bootstrap good luck! (dont forget the save button)
+    TO DO: use th api to limit results too display 2 pages 
+
+    TO DO: save button, add event listener to a button that look like a heart or paw print or if htat to hard just text
+    when the button is pressed it should store that animal into local storage as a sperate thing from userInfo, mayeb store as favResults
+    clicking favs should  NOT bring you to th favs page!!!!!
+
+
+    if we have time give this page a saved abailty so that the user can go back and forth between reults and saved cats
+
+*/
