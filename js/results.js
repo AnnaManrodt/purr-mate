83713239fd7a9480b6846c5481cfa1a7da808227
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-04-17 16:56:19 -0500

    formated the page back to make them pretty

--- js/results.js
+++ js/results.js
@@ -4,39 +4,39 @@ const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 if (userInfo) {
   // Query the API for matches for our cat
   const apiUrl = `https://api-staging.adoptapet.com/search/pet_search?key=hg4nsv85lppeoqqixy3tnlt3k8lj6o0c&v=3&output=json&city_or_zip=${userInfo.location}&geo_range=${userInfo.geoRange}&species=cat&sex=${userInfo.gender}&age=${userInfo.age}&color_id=${userInfo.color}&pet_size_range_id=&hair=${userInfo.hair}&bonded_pair=&special_needs=${userInfo.specialNeeds}&include_mixes=&added_after=&start_number=1&end_number=15&meta_only=0`
-  
+
   // Make call to API
   fetch(apiUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    // Creating match results array 
-    const catResults = data.pets;
-    
-    // Save the cat results to the localStorage
-    localStorage.setItem('catResults', JSON.stringify(catResults));
-    
-    if ('exception' in data) { // Check for no pets found
-      generateNoPetsMessage();
-    } else {
-      const favoriteCats = JSON.parse(localStorage.getItem('favoriteCats'));
-      
-      // Display the rows with the cat results
-      generateMatchRows(catResults);
-      
-      // Mark the entries that have been added as favorites
-      if (favoriteCats) {
-        favoriteCats.forEach((favoriteCat) => {
-          for (let i = 0; i < catResults.length; i++) {
-            if(catResults[i].pet_name.includes(favoriteCat.name)) {
-              $(`span[data-index="${i}"]`).removeAttr('hidden');
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // Creating match results array 
+      const catResults = data.pets;
+
+      // Save the cat results to the localStorage
+      localStorage.setItem('catResults', JSON.stringify(catResults));
+
+      if ('exception' in data) { // Check for no pets found
+        generateNoPetsMessage();
+      } else {
+        const favoriteCats = JSON.parse(localStorage.getItem('favoriteCats'));
+
+        // Display the rows with the cat results
+        generateMatchRows(catResults);
+
+        // Mark the entries that have been added as favorites
+        if (favoriteCats) {
+          favoriteCats.forEach((favoriteCat) => {
+            for (let i = 0; i < catResults.length; i++) {
+              if (catResults[i].pet_name.includes(favoriteCat.name)) {
+                $(`span[data-index="${i}"]`).removeAttr('hidden');
+              }
             }
-          } 
-        }) 
-      } 
-    }
-  })
+          })
+        }
+      }
+    })
 } else generateNoPetsMessage();
 
 
@@ -47,12 +47,12 @@ const resultsDivElement = $('.results');
 let favoriteCat = {};
 
 resultsDivElement.on('click', (event) => {
-  
-  
+
+
   const catResults = JSON.parse(localStorage.getItem('catResults'));
-  
+
   // create the favorite cat object being added
-  
+
   if (!jQuery.isEmptyObject($(event.target).data())) { // only execute if on a button with a data-index attribute
     favoriteCat = {
       name: catResults[$(event.target).data('index')].pet_name,
@@ -62,12 +62,12 @@ resultsDivElement.on('click', (event) => {
       age: catResults[$(event.target).data('index')].age,
       resident: `${catResults[$(event.target).data('index')].addr_city}, ${catResults[$(event.target).data('index')].addr_state_code}`,
     }
-    
-    
+
+
     // generate the list of favorite cats
     // check to see if there is an existing array with favorite cats.  If not then create a new array.
     let favoriteCats = JSON.parse(localStorage.getItem('favoriteCats'));
-    
+
     if (favoriteCats) {
       // Checking if the favorite cat already exists because we don't want to add another one of the same one to the list
       if (!favoriteCats.find((cat) => {
@@ -75,7 +75,7 @@ resultsDivElement.on('click', (event) => {
       })) {
         // Add to favorite list
         favoriteCats.push(favoriteCat);
-        
+
         // Show the favorite list icon for selected cats
         $(event.target).siblings('span').removeAttr('hidden');
         localStorage.setItem('favoriteCats', JSON.stringify(favoriteCats));
@@ -85,12 +85,12 @@ resultsDivElement.on('click', (event) => {
       })) {// remove favorites if they exist in the favorite array
         favoriteCats.filter((cat, index, array) => {
           if (cat.name === favoriteCat.name) {
-            array.splice(index,1);
+            array.splice(index, 1);
             localStorage.setItem('favoriteCats', JSON.stringify(favoriteCats));
             $(event.target).siblings('span').attr('hidden', '');
           }
         })
-      }  
+      }
     } else { // no favorite array yet so create one
       favoriteCats = [favoriteCat];
       localStorage.setItem('favoriteCats', JSON.stringify(favoriteCats));
@@ -105,13 +105,13 @@ resultsDivElement.on('click', (event) => {
 //////////
 function generateMatchRows(catResults) {
   const resultsDivElement = $('.results');
-  
+
   // Creating the elements
   const divContainerElement = $(`<div class="container">`);
-  
+
   for (let i = 0; i < catResults.length; i++) {
-    
-    
+
+
     const rowDivElement = $(`<div class="row text-left border">`)
     const firstRowDivElement = $(`<div class="row">`);
     const nameDivElement = $(`<div class="col-4 mb-3 catName">Name: ${catResults[i].pet_name}</div>`);
@@ -124,8 +124,8 @@ function generateMatchRows(catResults) {
     const thirdRowDivElement = $(`<div class="row justify-content-center mb-3">`);
     const addToFavButtonElement = $(`<button  type= "button" class="col-2 btn btn-outline-secondary" data-index=${i}>Add/Remove to Favorite</button>`)
     const heartImageElement = $(`<span class="col-1" hidden data-index=${i}>😻</span>`);
-    
-    
+
+
     // Appending them back to the HTML to the results div element
     firstRowDivElement.append(nameDivElement);
     firstRowDivElement.append(genderDivElement);
@@ -144,10 +144,10 @@ function generateMatchRows(catResults) {
 }
 
 function generateNoPetsMessage() {
-  const resultsDivElement = $('.results'); 
+  const resultsDivElement = $('.results');
   const rowDivElement = $(`<div class="row text-left">`)
   const messageH2Element = $(`<h2 >No Purr mate found 😿.  Please enter new search criteria on the Form page.</h2>`);
-  
+
   rowDivElement.append(messageH2Element);
   resultsDivElement.append(rowDivElement);
 }
